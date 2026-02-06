710470712a7bff016671b61f72353831070c1a0e
Laffin <michael.laffin@hpe.com>
2026-02-06 14:19:03 -0600

    determine installation root with env var

--- setup.tcl
+++ setup.tcl
@@ -1,5 +1,24 @@
+proc find_sigrity_root {} {
+    if {![info exists ::env(SIGRITY_EDA_DIR)]} {
+        error "SIGRITY_EDA_DIR environment variable not set"
+    }
+
+    set active_path [file normalize $::env(SIGRITY_EDA_DIR)]
+    
+    if {![file exists $active_path]} {
+        error "Sigrity installation path does not exist: $active_path"
+    }
+    
+    return $active_path
+}
 
 
+
+set SIGRITY_ROOT [find_sigrity_root]
+set MATERIAL_LIB_PATH "$SIGRITY_ROOT/share/pcb/text/material.cmx"
+set REQUIRED_MATERIALS {!RTF !HVLP}
+
+puts "Sigrity root: $SIGRITY_ROOT"
 puts "Updating all conductor layers to material: !RTF"
 # Reference: C:/Cadence/Sigrity2023.1/doc/pdc_ug/c9_TCL_Via_Material_Assignment_Commands.html
 if {[catch {sigrity::update layer model_name {!RTF} {all conductor layers} {!}} result]} {
