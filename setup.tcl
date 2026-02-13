b7d451f9ef0060698d111aedead7699f5745a198
Laffin <michael.laffin@hpe.com>
2026-02-13 14:44:15 -0600

    set global plating thickness to 1 mil

--- setup.tcl
+++ setup.tcl
@@ -5,7 +5,7 @@
 # source "C:/path/to/script/setup.tcl"
 
 
-# Determine install directory from environment variable
+# Find install directory from environment variable
 proc find_sigrity_root {} {
     if {![info exists ::env(SIGRITY_EDA_DIR)]} {
         error "SIGRITY_EDA_DIR environment variable not set"
@@ -51,12 +51,13 @@ proc validate_material_library {lib_path required_materials} {
 
 
 set SIGRITY_ROOT [find_sigrity_root]
+puts "Sigrity root: $SIGRITY_ROOT"
+
 set MATERIAL_LIB_PATH "$SIGRITY_ROOT/share/pcb/text/material.cmx"
 set REQUIRED_MATERIALS {!RTF !HVLP}
 
 validate_material_library $MATERIAL_LIB_PATH $REQUIRED_MATERIALS
 
-puts "Sigrity root: $SIGRITY_ROOT"
 
 puts "Updating all conductor layers to material: !RTF"
 # refer to: C:/Cadence/Sigrity2023.1/doc/pdc_ug/c9_TCL_Via_Material_Assignment_Commands.html
@@ -66,6 +67,24 @@ if {[catch {sigrity::update layer model_name {!RTF} {all conductor layers} {!}} 
 } else {
     puts "Set all conductor layers to material: !RTF"
 }
+
+# Set plating thickness to 1 mil
+if {[catch {sigrity::update option -GlobalUnit {mil} {!}} result]} {
+    puts "ERROR: Failed to set global unit to mil: $result"
+} else {
+    if {[catch {sigrity::update PadStack -all -PlatingThickness {1} {!}} result]} {
+        puts "ERROR: Failed to set padstack plating thickness: $result"
+    } else {
+        puts "Set all padstack plating thickness to: 1mil"
+    }
+}
+
+# -----------------------------------------------------------------------
+# Simulation Options
+# -----------------------------------------------------------------------
+
+# Turn off accuracy mode initially (required for -makePadsEquipotential)
+sigrity::set pdcAccuracyMode {0}
 
 if {[catch {sigrity::update option -treatPadAsShape {1}} result]} {
     puts "ERROR: Command failed with message: $result"
@@ -78,4 +97,3 @@ if {[catch {sigrity::update option -makePadsEquipotential {1}} result]} {
 } else {
     puts "Enabled makePadsEquipotential option"
 }
-
